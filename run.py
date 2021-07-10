8a5e206fc6748aef45199369f5bbf5442fd99518
AndyB-WHG <andrew.bond2@gmail.com>
2021-07-10 09:03:35 +0000

    Added function to calculate stock needed for the next day's market.

--- run.py
+++ run.py
@@ -104,6 +104,22 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(last_5_days_data):
+    """
+    Take last five days sales data and find the average for each sandwich, adding 10%.
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in last_5_days_data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -113,10 +129,13 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    print(stock_data)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Data Automation")
 
 
-# main()
-sales_columns = get_last_5_entries_sales()
+main()
